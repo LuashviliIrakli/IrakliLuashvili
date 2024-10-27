b53a064e14a9a7ac7d640074aaedf8b64c006740
Irakli Luashvili <iluashvili@credo.ge>
2024-10-27 14:15:56 +0400

    Add first home work

---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,29 @@
+﻿
+internal class Program
+{
+    static void Main(String[] args)
+    {
+        Console.WriteLine("Hello World");
+
+        string name = "Irakli";
+
+        string lastName = "Luashvili";
+
+        byte age = 35;
+
+        string personalNumber = "350010";
+
+        string phoneNumber = "5937675";
+
+        short footSize = 43;
+
+        double weight = 83.3;
+
+        float height = 1.77F;
+
+        bool isGeorgian = true;
+
+
+    }
+
+}
