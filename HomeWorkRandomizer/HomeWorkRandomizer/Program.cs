415cdf55787ce9a396a4afb298f17d1b4b257816
Irakli Luashvili <iluashvili@credo.ge>
2024-10-27 18:58:44 +0400

    add home work rendomizer

---
+++ HomeWorkRandomizer/HomeWorkRandomizer/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace HomeWorkRandomizer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int importedNumber1 = int.Parse(Console.ReadLine());
+
+            Random random = new Random();
+            int randomNumber = random.Next(1, 10);
+            if (importedNumber1 == randomNumber)
+            {
+                Console.WriteLine("Cifri gamoicanit");
+            }
+            else
+            {
+                Console.WriteLine("gtxovt scadot tavivad");
+                int importedNumber2 = int.Parse(Console.ReadLine());
+                if (importedNumber2 == randomNumber)
+                {
+                    Console.WriteLine("Cifri gamoicanit");
+                }
+                else
+                {
+                    Console.WriteLine("gtxovt scadot tavivad");
+                    int importedNumber3 = int.Parse(Console.ReadLine());
+                    if (importedNumber3 == randomNumber)
+                    {
+                        Console.WriteLine("Cifri gamoicanit");
+                    }
+                    else
+                    {
+                        Console.WriteLine("tamashi waaget");
+                    }
+                }
+
+            }
+        }
+    }
+}
+
