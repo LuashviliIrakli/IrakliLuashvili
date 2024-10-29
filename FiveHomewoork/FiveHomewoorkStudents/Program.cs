66f47688bc0729b57350f2789d5cfde404051913
Irakli Luashvili <iluashvili@credo.ge>
2024-10-29 19:32:24 +0400

    Add Five Homework

---
+++ FiveHomewoork/FiveHomewoorkStudents/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace FiveHomewoorkStudents
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] students = new string[3];
+            for (int i = 0; i < students.Length; i++)
+            {
+                students[i] = Console.ReadLine();
+            }
+            for (int i = 0; i < students.Length; i++)
+            {
+                Console.WriteLine(students[i]);
+            }
+        }
+    }
+}
---
+++ FiveHomewoork/FiveHomeworkGrades/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace FiveHomeworkGrades
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] students = new string[3];
+            int[,] grades = new int[3, 2];
+
+            for (int i = 0; i < students.Length; i++)
+            {
+                Console.WriteLine("sheiyvanet studentis saxeli");
+                students[i] = Console.ReadLine();
+
+                Console.WriteLine("sheiyvanet qulebi");
+
+                for (int j = 0; j < 2; j++)
+                {
+                    Console.Write($"sagani {j + 1}: ");
+                    grades[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine("studentis qulebi: ");
+            for (int i = 0; i < students.Length; i++)
+            {
+                Console.WriteLine($"{students[i]}: qulebi - {grades[i, 0]}, {grades[i, 1]}");
+            }
+        }
+    }
+}
