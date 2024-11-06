e8a7c333073bc2c3578252024a94b3ef942694da
Irakli Luashvili <iluashvili@credo.ge>
2024-11-06 22:46:48 +0400

    Sixth Homework

---
+++ SixthHomework/SixthHomework/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace SixthHomework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   
+            Console.WriteLine("Sheiyvanet mteli ricxvebis masivi mdzimit(,) gamoyofili");
+            string input = Console.ReadLine();
+            string[] stringArray = input.Split(',');
+            int[] array = new int[stringArray.Length];
+
+            for (int i = 0; i < stringArray.Length; i++)
+            {
+                array[i] = int.Parse(stringArray[i].Trim());
+            }
+
+            Console.WriteLine("Sheiyvanet indexi");
+            int index = int.Parse(Console.ReadLine());
+
+            int result = SumOfDigitsAtIndex(array, index);
+            Console.WriteLine($"Sum: {result}");
+        }
+
+        static int SumOfDigitsAtIndex(int[] array, int index)
+        {
+            int number = array[index];
+            int sum = 0;
+            while (number != 0)
+            {
+                sum += number % 10;
+                number /= 10;
+            }
+
+            return sum;
+        }
+    }
+}
