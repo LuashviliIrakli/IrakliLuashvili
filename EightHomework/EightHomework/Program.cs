198710af6a4e36642c98824087bba9672afae0fe
Irakli Luashvili <iluashvili@credo.ge>
2024-11-13 00:54:46 +0400

    Add Eight Homework

---
+++ EightHomework/EightHomework/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace EightHomework
+{
+    class Program
+    {
+        public class Person
+        {
+            public string Name;
+            public int Age;
+        }
+
+        static void Main(string[] args)
+        {
+            string personName = "Irakli";
+            int personAge = 35;
+
+            Person person1 = new Person();
+            person1.Name = personName;
+            person1.Age = personAge;
+
+            Person person2 = person1;
+
+            person2.Name = "Giorgi";
+            person2.Age = 30;
+        }
+    }
+}
