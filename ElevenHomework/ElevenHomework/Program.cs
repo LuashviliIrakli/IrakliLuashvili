7f7f726145fd04c7d4bd61c8e2363a7c22070913
Irakli Luashvili <iluashvili@credo.ge>
2024-11-22 16:56:11 +0400

    Add ElevenHomework N1

---
+++ ElevenHomework/ElevenHomework/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace ElevenHomework
+{
+    public class Counter
+    {
+        private static int Count = 0;
+
+        public Counter()
+        {
+            Count++;
+        }
+
+        public static int GetCount()
+        {
+            return Count;
+        }
+
+        public static void ResetCount()
+        {
+            Count = 0;
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Counter counter1 = new Counter();
+            Console.WriteLine($"Count: {Counter.GetCount()}");
+
+            Counter counter2 = new Counter();
+            Console.WriteLine($"Count: {Counter.GetCount()}");
+
+            Counter counter3 = new Counter();
+            Console.WriteLine($"Count: {Counter.GetCount()}");
+
+            Counter.ResetCount();
+            Console.WriteLine($"Count after reset: {Counter.GetCount()}");
+
+            Counter counter4 = new Counter();
+            Console.WriteLine($"Count after new object: {Counter.GetCount()}");
+        }
+    }
+}
